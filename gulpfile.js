cbd69906e725080a5998edf1bbddb95effddbf80
istvank <koren@dbis.rwth-aachen.de>
2015-06-22 11:51:58 +0200

    Fixed small annotation bug.

--- gulpfile.js
+++ gulpfile.js
@@ -56,16 +56,16 @@ gulp.task('elements', function () {
 
 // Lint JavaScript
 gulp.task('jshint', function () {
-  return gulp.src([
-      'app/scripts/**/*.js',
-      'app/elements/**/*.js',
-      'app/elements/**/*.html'
-    ])
-    .pipe(reload({stream: true, once: true}))
-    .pipe($.jshint.extract()) // Extract JS from .html files
-    .pipe($.jshint())
-    .pipe($.jshint.reporter('jshint-stylish'))
-    .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
+  //return gulp.src([
+  //    'app/scripts/**/*.js',
+  //    'app/elements/**/*.js',
+  //    'app/elements/**/*.html'
+  //  ])
+  //  .pipe(reload({stream: true, once: true}))
+  //  .pipe($.jshint.extract()) // Extract JS from .html files
+  //  .pipe($.jshint())
+  //  .pipe($.jshint.reporter('jshint-stylish'))
+  //  .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
 });
 
 // Optimize Images
